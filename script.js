2b107be48dd9025baf0902a8d1c717462e5184aa
Ilann STEFANOVITCH <ilann.stefanovitch@etu.univ-amu.fr>
2026-01-22 21:07:52 +0100

    E 7

--- script.js
+++ script.js
@@ -12,30 +12,50 @@ function setup() {
 
 function AfficherLaTemperature() {
   ++I_I;
-  if (I_I >= A_tempe.length - 1) {
+  if (I_I >= A_tempe.length) {
     I_I = 0;
   }
 
-  var O_AfficheTemp = document.getElementById("tempList");
-  O_AfficheTemp.textContent = A_tempe[I_I];
+  var currentTemp = A_tempe[I_I];
+
+  // Mise à jour sémantique
+  var O_TempValue = document.getElementById("tempValue");
+  var O_History = document.getElementById("history");
+
+  O_TempValue.textContent = currentTemp;// Mise à jour du contenu textuel
+  O_TempValue.setAttribute("value", currentTemp);// Mise à jour de l'attribut value
+  O_History.textContent +=" "+ currentTemp+ "°C"; // Ajout de la température à l'historique
 
   var O_AfficheMesage = document.getElementById("message");
 
-  if (A_tempe[I_I] < 0) {
+  if (currentTemp < 0) {
     O_AfficheMesage.textContent = "Brrrrrrr, un peu froid ce matin, mets ta cagoule !";
-    O_AfficheTemp.className = A_TemperatureCategories[0];
-  } else if (A_tempe[I_I] >= 0 && A_tempe[I_I] <= 20) {
+    O_TempValue.className = A_TemperatureCategories[0];
+  }
+  else if (currentTemp >= 0 && currentTemp <= 20) {
     O_AfficheMesage.textContent = "";
-    O_AfficheTemp.className = A_TemperatureCategories[1];
-  } else if (A_tempe[I_I] > 20 && A_tempe[I_I] <= 30) {
-    O_AfficheMesage.textContent = "";
-    O_AfficheTemp.className = A_TemperatureCategories[2];
-  } else {
+    O_AfficheMesage.
+    O_TempValue.className = A_TemperatureCategories[1];
+  }
+  else if (currentTemp > 20 && currentTemp <= 30) {
+    O_AfficheMesage.textContent = " ";
+    O_TempValue.className = A_TemperatureCategories[2];
+  }
+  else {
     O_AfficheMesage.textContent = "Caliente ! Vamos a la playa, ho hoho hoho ";
-    O_AfficheTemp.className = A_TemperatureCategories[3];
+    O_TempValue.className = A_TemperatureCategories[3];
+  }
+}
+
+function AfficherHistorique() {
+
+  O_History.innerHTML = ""; // On vide avant de remplir
+  for (var i = 0; i < A_tempe.length; i++) {
+
   }
 }
 
 const intervalID = setInterval(AfficherLaTemperature, 500);
 
 setup();
+AfficherHistorique();
